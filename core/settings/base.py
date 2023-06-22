d75f13fbc9a65fa3d5734543c184d0a212b5c9cd
harsh <2028202@kiit.ac.in>
2023-06-22 22:04:23 +0530

    change

--- core/settings/base.py
+++ core/settings/base.py
@@ -95,7 +95,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 
 MEDIA_URL = "/media/"
